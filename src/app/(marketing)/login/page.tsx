8e0cf126b3243391cb2958f874cf79e319b44457
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2026-02-27 19:32:06 +0530

    feat: implement authentication with better-auth

--- src/app/(marketing)/login/page.tsx
+++ src/app/(marketing)/login/page.tsx
@@ -1,11 +1,29 @@
 import { LoginForm } from "@/components/auth/login-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function LoginPage() {
-    return(
-        <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+  return (
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-md">
-        <LoginForm />
+        <Card className="bg-muted">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            {/* login form  */}
+            <LoginForm />
+          </CardContent>
+        </Card>
       </div>
     </main>
-    )
+  )
 }
