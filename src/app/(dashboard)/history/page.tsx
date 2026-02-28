9f62eb36abbfdcaf020e63da4411ca1c4403d604
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2026-02-28 20:21:58 +0530

    refactor: add dashboard pages and sidebar navigation links for improved user experience

---
+++ src/app/(dashboard)/history/page.tsx
@@ -0,0 +1,9 @@
+
+export default function HistoryPage() {
+    return (
+        <div className="flex items-center justify-center w-full h-[93vh] flex-col gap-4">
+            <h1 className="text-2xl sm:text-4xl text-primary font-extrabold">History</h1>
+            <p className="text-sm sm:text-lg mx-auto w-md text-center">this is history page show query history for querymate</p>
+        </div>
+    )
+};
--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -3,13 +3,14 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
-    SidebarGroupAction,
-    SidebarGroupContent,
-    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { Plus } from "lucide-react"
+import { CircleQuestionMark } from "lucide-react"
 import Logo from "./logo"
+import NavLinks from "./nav-links"
 
 export function AppSidebar() {
     return (
@@ -19,14 +20,29 @@ export function AppSidebar() {
             </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupAction>
-                        <Plus /> <span className="sr-only">Add Project</span>
-                    </SidebarGroupAction>
-                    <SidebarGroupContent>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus deserunt sed esse, rerum dolorem quam dolores laudantium, ex, facere inventore nesciunt cum natus? Consectetur esse, natus velit blanditiis optio porro voluptates tempore explicabo magnam, autem nisi consequatur ipsum dolorem debitis dolorum adipisci cumque rem dignissimos? Ex expedita, a dolor libero, optio vero tempore earum sunt culpa quidem tenetur quas obcaecati inventore ipsum. Deserunt tempora dolores officia omnis.</SidebarGroupContent>
+                    {/* Nav Links  */}
+                    <NavLinks />
                 </SidebarGroup>
             </SidebarContent>
-            <SidebarFooter />
+            <SidebarFooter className="border-t shadow">
+                <SidebarMenu >
+                    <SidebarMenuItem >
+                        <SidebarMenuButton
+                            asChild
+                            tooltip={"Support"}
+                            // isActive={pathname.startsWith(link.url)}
+                            className="data-[active=true]:bg-primary data-[active=true]:text-background mx-auto  w-[90%]"
+                        >
+                            <div className="py-5 flex gap-3 text-base sm:text-[15px] px-3 font-medium ">
+                                <span>
+                                    <CircleQuestionMark className="size-5 sm:size-6" />
+                                </span>
+                                <span>Support</span>
+                            </div>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
         </Sidebar>
     )
 }
