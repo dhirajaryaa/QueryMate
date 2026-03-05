e21ef906a2e4acc874ff372a6b725e422f84d2cd
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2026-03-05 20:33:22 +0530

    feat: chat load on message page from db

--- src/app/(app)/chat/[chatId]/page.tsx
+++ src/app/(app)/chat/[chatId]/page.tsx
@@ -1,56 +1,82 @@
 "use client";
 import ChatInput from "@/components/chat/chat-input";
-import { AssistantChatMessage, UserChatMessage } from "@/components/chat/chat-message";
+import {
+  AssistantChatMessage,
+  UserChatMessage,
+} from "@/components/chat/chat-message";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
+import { toast } from "sonner";
 
-export default function ChatPage() {
+export default function ChatPage({
+  params,
+}: {
+  params: Promise<{ chatId: string }>;
+}) {
+  const { chatId } = use(params);
+  const [messages, setMessages] = useState<any[]>([]);
+  const router = useRouter();
 
-    const sendMessage = (message: string) => {
-        console.log("Form submitted 😊 =>", message);
+  async function getUserChat() {
+    try {
+      const res = await fetch(`/api/chat/${chatId}/message`);
 
-        return message;
-    };
+      const data = await res.json();
 
-const message = `
-# Markdown Test
+      if (!res.ok) {
+        toast.error(data.message || data.error);
+        return;
+      }
 
-Hello **world**
+      //data
+      if(!data.data){
+        toast.error("Chat not found!");
+        router.push("/new");
+      };
+      const fetchedMessages = data.data;
+      setMessages(fetchedMessages);
 
-Inline code: \`const x = 10\`
+      const lastMessage = fetchedMessages.at(-1);
 
-## JavaScript Example
+      if (lastMessage && lastMessage.role === "user") {
+        //! call llm
+        console.log("calling LLM code here");
+      }
 
-\`\`\`js
-function greet(name) {
-  console.log("Hello " + name);
-}
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-greet("Dhiraj")
-\`\`\`
+  const sendMessage = (message: string, dbId: string) => {
+    console.log("Form submitted 😊 =>", message, dbId);
 
-## List
+    return message;
+  };
 
-- item 1
-- item 2
-- item 3
-`;
+  // call to get chats
+  useEffect(() => {
+    getUserChat();
+  }, [chatId]);
 
-    return (
-        <div className="w-full flex flex-1 flex-col items-center overflow-hidden">
-            <section className="flex flex-1 flex-col w-full h-full overflow-y-auto">
-                {/* chat list  */}
-                <div className="flex flex-col gap-4 flex-1 w-full max-w-3xl mx-auto px-6 py-4">
-                    {/* user message  */}
-                    <UserChatMessage message={`who are you?`} />
-
-                    {/* assistant  message  */}
-                    <AssistantChatMessage message={message} />
-
-                </div>
-            </section>
-            {/* input box  */}
-            <section className="w-full sticky bottom-0 bg-background z-1 max-w-3xl mx-auto px-4 pb-4">
-                <ChatInput sendMessage={sendMessage} />
-            </section>
+  return (
+    <div className="w-full flex flex-1 flex-col items-center overflow-hidden">
+      <section className="flex flex-1 flex-col w-full h-full overflow-y-auto">
+        {/* chat list  */}
+        <div className="flex flex-col gap-4 flex-1 w-full max-w-3xl mx-auto px-6 py-4">
+          {messages?.map((message) =>
+            message.role === "user" ? (
+              <UserChatMessage message={message.content} key={message.id}/>
+            ) : (
+              <AssistantChatMessage message={message.content} key={message.id}/>
+            ),
+          )}
         </div>
-    )
-};
+      </section>
+      {/* input box  */}
+      <section className="w-full sticky bottom-0 bg-background z-1 max-w-3xl mx-auto px-4 pb-4">
+        <ChatInput sendMessage={sendMessage} />
+      </section>
+    </div>
+  );
+}
