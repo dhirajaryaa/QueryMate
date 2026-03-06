784ae9a631ac2802f3475d6dc47cfb662b232b80
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2026-03-06 16:38:08 +0530

    feat: llm chat stream add

--- src/app/(app)/chat/[chatId]/page.tsx
+++ src/app/(app)/chat/[chatId]/page.tsx
@@ -22,17 +22,12 @@ export default function ChatPage({
       const res = await fetch(`/api/chat/${chatId}/message`);
 
       const data = await res.json();
-
       if (!res.ok) {
         toast.error(data.message || data.error);
         return;
       }
 
       //data
-      if(!data.data){
-        toast.error("Chat not found!");
-        router.push("/new");
-      };
       const fetchedMessages = data.data;
       setMessages(fetchedMessages);
 
@@ -41,17 +36,57 @@ export default function ChatPage({
       if (lastMessage && lastMessage.role === "user") {
         //! call llm
         console.log("calling LLM code here");
-      }
 
+        sendMessage(lastMessage.content);
+      }
     } catch (error) {
       console.error(error);
     }
   }
 
-  const sendMessage = (message: string, dbId: string) => {
-    console.log("Form submitted 😊 =>", message, dbId);
+  const sendMessage = async (message: string) => {
+    try {
+    setMessages((prev) => [
+      ...prev,
+      { id: Date.now(), role: "user", content: message },
+    ]);
+  
 
-    return message;
+      const res = await fetch(`/api/chat/${chatId}/message`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: message }),
+      });
+
+      if (!res.body) {
+        throw new Error("no response form body");
+      }
+      let assistantMessage = "";
+      const reader = res.body?.getReader();
+      const decoder = new TextDecoder();
+
+      const tempId = Date.now();
+      setMessages((prev) => [
+        ...prev,
+        { id: tempId, role: "assistant", content: "" },
+      ]);
+
+      while (true) {
+        const { done, value } = await reader?.read();
+        if (done) break;
+        const chunk = decoder.decode(value, { stream: true });
+        assistantMessage += chunk;
+        setMessages((prev) =>
+          prev.map((msg) =>
+            msg.id === tempId ? { ...msg, content: assistantMessage } : msg,
+          ),
+        );
+      }
+    } catch (error) {
+      console.error("Network error", error);
+    }
   };
 
   // call to get chats
@@ -66,9 +101,12 @@ export default function ChatPage({
         <div className="flex flex-col gap-4 flex-1 w-full max-w-3xl mx-auto px-6 py-4">
           {messages?.map((message) =>
             message.role === "user" ? (
-              <UserChatMessage message={message.content} key={message.id}/>
+              <UserChatMessage message={message.content} key={message.id} />
             ) : (
-              <AssistantChatMessage message={message.content} key={message.id}/>
+              <AssistantChatMessage
+                message={message.content}
+                key={message.id}
+              />
             ),
           )}
         </div>
---
+++ src/prompt/system-prompt.ts
@@ -0,0 +1,8 @@
+export const SYSTEM_PROMPT = `
+You are QueryMate AI.
+You help users query databases and answer questions.
+Rules:
+- Generate safe SQL queries
+- Never delete data unless user confirms
+- Explain the SQL query before returning results
+`;
